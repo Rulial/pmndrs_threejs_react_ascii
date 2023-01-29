03a23646ef37becc0a26d422fb063db77f77a312
Rulial <98750732+Rulial@users.noreply.github.com>
2023-01-29 12:26:51 +0000

    update

--- src/App.js
+++ src/App.js
@@ -51,14 +51,14 @@ function AsciiRenderer({
   const { size, gl, scene, camera } = useThree()
 
   // Create effect
-  const effect = useMemo(() => {
-    const effect = new AsciiEffect(gl, characters, { invert, color, resolution })
-    effect.domElement.style.position = 'relative'
-    effect.domElement.style.top = '0px'
-    effect.domElement.style.left = '0px'
-    effect.domElement.style.pointerEvents = 'none'
-    return effect
-  }, [characters, invert, color, resolution])
+  //const effect = useMemo(() => {
+    //const effect = new AsciiEffect(gl, characters, { invert, color, resolution })
+    //effect.domElement.style.position = 'relative'
+    //effect.domElement.style.top = '0px'
+    //effect.domElement.style.left = '0px'
+    //effect.domElement.style.pointerEvents = 'none'
+    //return effect
+  //}, [characters, invert, color, resolution])
 
   // Styling
   useLayoutEffect(() => {
