91e20faa65dd7c61a76895464ea0bb9ca5a01578
Rulial <98750732+Rulial@users.noreply.github.com>
2023-01-29 11:19:17 +0000

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
       <color attach="background" args={['black']} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <Torusknot />
+      <Sphere />
       <OrbitControls />
       <AsciiRenderer fgColor="white" bgColor="black" />
     </Canvas>
