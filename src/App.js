b403a283cf51593595fe79de3b887db7b32d3185
Rulial <98750732+Rulial@users.noreply.github.com>
2023-01-29 13:07:12 +0000

    update

--- src/App.js
+++ src/App.js
@@ -54,8 +54,8 @@ function AsciiRenderer({
   const effect = useMemo(() => {
     const effect = new AsciiEffect(gl, characters, { invert, color, resolution })
     effect.domElement.style.position = 'absolute'
-    /*effect.domElement.style.top = '0px'
-    effect.domElement.style.left = '0px' */
+    effect.domElement.style.top = '0px'
+    effect.domElement.style.left = '0px'
     effect.domElement.style.pointerEvents = 'none' 
     return effect
   }, [characters, invert, color, resolution])
