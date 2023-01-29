d51f8d1bd8a234576e88e2de793906fcb14db324
Rulial <98750732+Rulial@users.noreply.github.com>
2023-01-29 13:43:21 +0000

    update

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function Box(props) {
       onClick={() => click(!clicked)}
       onPointerOver={() => hover(true)}
       onPointerOut={() => hover(false)}>
-      <cubeGeometry args={[5, 5, 5]} />
+      <BoxGeometry args={[5, 5, 5]} />
       <meshStandardMaterial color="orange" />
     </mesh>
   )
